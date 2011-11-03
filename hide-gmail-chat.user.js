6c3b44ea101fc2e2f27f103bdeb8af31e550669b
Davanum Srinivas <davanum@gmail.com>
2011-11-03 13:38:40 -0400

    support firefox

--- hide-gmail-chat.user.js
+++ hide-gmail-chat.user.js
@@ -1,20 +1,21 @@
 // ==UserScript==
 // @name           Hide Gmail Chat
+// @namespace      https://github.com/dims/greasemonkey-scripts/
 // @include        https://mail.google.com/*
 // @include        http://mail.google.com/*
 // ==/UserScript==
 
 var css = "@namespace url(http://www.w3.org/1999/xhtml); .pS.s { display:none !important; } ";
 if (typeof GM_addStyle != "undefined") {
-	GM_addStyle(css);
+    GM_addStyle(css);
 } else if (typeof addStyle != "undefined") {
-	addStyle(css);
+    addStyle(css);
 } else {
-	var heads = document.getElementsByTagName("head");
-	if (heads.length > 0) {
-		var node = document.createElement("style");
-		node.type = "text/css";
-		node.appendChild(document.createTextNode(css));
-		heads[0].appendChild(node); 
-	}
+    var heads = document.getElementsByTagName("head");
+    if (heads.length > 0) {
+        var node = document.createElement("style");
+        node.type = "text/css";
+        node.appendChild(document.createTextNode(css));
+        heads[0].appendChild(node);
+    }
 }
